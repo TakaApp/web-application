e1db0ba4b907665f761f9e3df16702b8ff457041
Aksels Ledins <aksels.ledins@gmail.com>
2018-04-15 21:50:40 +0200

    (atoms) +hour

---
+++ src/atoms/hour.vue
@@ -0,0 +1,22 @@
+<template>
+  <span>
+    {{format(data)}}
+  </span>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+  name: 'Hour',
+  props: ['data'],
+  methods: {
+    format: (timestamp) => {
+      return `${moment(timestamp).format('HH:mm')}`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
