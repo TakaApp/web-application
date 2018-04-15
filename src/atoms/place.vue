62ba3b2acc68d7658e45cf68f78e755276afe100
Aksels Ledins <aksels.ledins@gmail.com>
2018-04-15 23:08:21 +0200

    (atoms) +place

---
+++ src/atoms/place.vue
@@ -0,0 +1,23 @@
+<template>
+  <span>
+    {{name(data)}}
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'Place',
+  props: ['data'],
+  methods: {
+    name: (data) => {
+      if (data.name.toLowerCase() === 'origin') {
+        return 'Point de départ';
+      }
+      return data.name;
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
