d4e1294b300df5d9b5818af10116f47fb863f9f8
Aksels Ledins <aksels.ledins@gmail.com>
2018-04-15 20:31:03 +0200

    (atoms) +duration

---
+++ src/atoms/duration.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    {{duration(data)}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Duration',
+  props: ['data'],
+  methods: {
+    duration: (durationInSeconds) => {
+      const date = new Date(null);
+      date.setSeconds(durationInSeconds);
+      return `Durée: ${date.toISOString().substr(11, 5)}`;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
