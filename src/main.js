5e67a42249b575648c74204d673cb2e804f07cf4
Aksels Ledins <aksels.ledins@gmail.com>
2018-04-15 22:33:25 +0200

    (main.js) register new atoms & moment library

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import moment from 'moment'
+
 import App from './App';
 import router from './router';
 
+import Duration from './atoms/duration';
+import Distance from './atoms/distance';
+import Hour from './atoms/hour';
+
 Vue.config.productionTip = false;
+
+moment.locale('fr');
+
+Vue.prototype.moment = moment
+
+Vue.component('Duration', Duration);
+Vue.component('Distance', Distance);
+Vue.component('Hour', Hour);
 
 /* eslint-disable no-new */
 new Vue({
@@ -13,3 +27,4 @@ new Vue({
   components: { App },
   template: '<App/>',
 });
+
