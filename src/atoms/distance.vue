96f7f6ec3d63c46c6c8936238ba258b3d05ebc15
Aksels Ledins <aksels.ledins@gmail.com>
2018-04-15 21:50:59 +0200

    (atoms) +distance

---
+++ src/atoms/distance.vue
@@ -0,0 +1,20 @@
+<template>
+  <span>
+    {{distance(data)}}
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'Distance',
+  props: ['data'],
+  methods: {
+    distance: (distanceInMeters) => {
+      return `${Math.round(distanceInMeters)}m`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
