2daf482798e4bc4590da40a42be4ecb8738e27d7
Aksels Ledins <aksels.ledins@gmail.com>
2018-04-15 23:08:35 +0200

    (atoms) +direction

---
+++ src/atoms/direction.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="place-container">
+    <div class="icon"></div>
+    <div>{{data}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Direction',
+  props: ['data'],
+};
+</script>
+
+<style scoped>
+
+  .direction-container {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .icon {
+    height: .85rem;
+    width: .85rem;
+    background-image: url(../assets/icons/direction.png);
+    background-repeat: no-repeat;
+
+    background-size: contain;
+    background-position: center center;
+    align-self: center;
+
+    margin-right: .5rem;
+  }
+
+</style>
