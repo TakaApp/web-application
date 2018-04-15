0bacb088d2cfdbdc0f8491eb9323c9a02c186ede
Aksels Ledins <aksels.ledins@gmail.com>
2018-04-15 22:32:52 +0200

    (app.vue) general css : leg-{container,description} semi general

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,34 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  color: #424242;
+}
+
+.leg-illustration {
+  flex: 2;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  border: 1px solid black;
+}
+.leg-description {
+  flex: 7;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  border: 1px solid black;
+
+  padding-left: 1rem;
+  padding-right: 1rem;
+}
+.leg-container {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  max-width: 500px;
+  margin: 0 auto;
 }
 </style>
