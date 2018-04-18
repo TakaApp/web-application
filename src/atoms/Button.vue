c873160204d4297f26f262f203afb147e0c36c37
Aksels Ledins <aksels.ledins@gmail.com>
2018-04-18 19:00:58 +0200

    (atoms) +Button

---
+++ src/atoms/Button.vue
@@ -0,0 +1,27 @@
+<template>
+  <button v-on:click="onClick">
+    {{label}}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: [
+    'label',
+    'onClick',
+  ],
+};
+</script>
+
+<style scoped>
+
+button {
+  padding: .5rem .25rem;
+  background-color: #8fcccb;
+  font-weight: bold;
+  border: none;
+  cursor: pointer;
+}
+
+</style>
