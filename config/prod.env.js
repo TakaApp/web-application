b7694a97553bf9a2650f0ddeff91bf0cbd851346
Aksels Ledins <aksels.ledins@gmail.com>
2018-04-18 20:29:03 +0200

    (all) use https for api & bump to v0.5.1

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"http://taka-api.aksels.io"',
-  VERSION: '"v0.5"'
+  API_URL: '"https://taka-api.aksels.io"',
+  VERSION: '"v0.5.1"'
 }
