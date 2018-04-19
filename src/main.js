d7256344e597564714a05f21d8d0651693b1fd42
Aksels Ledins <aksels.ledins@gmail.com>
2018-04-19 21:27:34 +0200

    (main.js) use element-ui TimeSelect, Select, DatePicker, Option

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import Vue from 'vue';
 import moment from 'moment';
 import L from 'leaflet';
 
+import { TimeSelect, Select, DatePicker, Option } from 'element-ui';
+import lang from 'element-ui/lib/locale/lang/fr';
+import locale from 'element-ui/lib/locale';
+
 import 'leaflet/dist/leaflet.css';
 
 import App from './App';
@@ -15,6 +19,9 @@ import Distance from './atoms/distance';
 import Hour from './atoms/hour';
 import Place from './atoms/place';
 import Direction from './atoms/direction';
+
+
+locale.use(lang);
 
 // eslint-disable-next-line
 console.log(`%cVersion ${process.env.VERSION}`, 'background: #222; color: #bada55');
@@ -40,6 +47,11 @@ Vue.component('Distance', Distance);
 Vue.component('Hour', Hour);
 Vue.component('Place', Place);
 Vue.component('Direction', Direction);
+
+Vue.use(TimeSelect);
+Vue.use(Select);
+Vue.use(DatePicker);
+Vue.use(Option);
 
 /* eslint-disable no-new */
 new Vue({
