aa3803d0b41f9ad09f47b6fff593ca99accc99ec
Aksels Ledins <aksels.ledins@gmail.com>
2018-04-19 19:55:18 +0200

    (a) Button : feels more natural

--- src/atoms/Button.vue
+++ src/atoms/Button.vue
@@ -24,6 +24,17 @@ button {
   font-weight: bold;
   border: none;
   cursor: pointer;
+  width: 100%;
+  box-shadow: 0 4px 6px hsla(0, 0%, 0%, 0.2);
 }
+
+button:focus {
+  outline: 0;
+}
+
+button:active {
+  box-shadow: none;
+}
+
 
 </style>
