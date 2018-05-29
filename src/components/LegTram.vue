733018cfe425423d5a4b28f21442ccc2fd4808d5
Aksels Ledins <aksels.ledins@gmail.com>
2018-05-29 20:39:15 +0200

    (c) +LegTram

---
+++ src/components/LegTram.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="leg-container">
+    <div
+      class="leg-illustration"
+      :style="{
+        paddingTop: first ? '2rem' : 0,
+        paddingBottom: last ? '2rem' : 0,
+      }"
+    >
+      <div class="tram">
+        <div class="icon"></div>
+        <div class="tram-number-container">
+          <div
+            class="tram-number"
+            :style="{
+              backgroundColor: `#${leg.routeColor}`,
+              color: `#${leg.routeTextColor}`,
+            }"
+          >
+            {{leg.route}}
+          </div>
+        </div>
+      </div>
+      <div class="line" />
+      <div class="end-time"><Hour :data="leg.endTime" /></div>
+    </div>
+    <div class="leg-description from">
+      <div v-if="index === 0" class="leg-description-item">
+        <b><Place :data="leg.from" /></b>
+      </div>
+      <div class="leg-description-item">
+        <div>
+          <b><Direction :data="leg.headSign" /></b>
+          <div class="next-departures">
+            Prochains départs:
+            <Hour :data="leg.startTime" />
+          </div>
+        </div>
+        <div><Duration :data="leg.duration" /></div>
+      </div>
+
+      <div class="leg-description-item stops">
+        <div>{{leg.to.stopSequence - leg.from.stopSequence}} arrêts</div>
+      </div>
+
+      <div class="leg-description-item">
+        <b><Place :data="leg.to" /></b>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'LegTram',
+  props: ['leg', 'index', 'last', 'first'],
+};
+</script>
+
+<style scoped>
+
+.line:after {
+  content: '';
+  height: 12px;
+  width: 12px;
+  border-radius: 50%;
+
+  background: #dbdbdb;
+  position: absolute;
+  margin-top: 4px;
+  bottom: -10px;
+  left: -6px;
+}
+.line {
+  position: relative;
+  width: 0;
+  border: 1px solid #dbdbdb;
+  flex-grow: 1;
+  margin-top: 1rem;
+  margin-bottom: 1.5rem;
+}
+
+.from {
+  padding-top: .25rem;
+}
+.next-departures {
+  font-size: .9rem;
+  padding-left: 1.25rem;
+}
+
+.tram {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+
+  align-self: flex-start;
+
+  align-content: center;
+  justify-content: center;
+  align-items: center;
+  margin-top: .6rem;
+}
+.tram-number-container {
+  min-width: 1.25rem;
+  margin-left: 2px;
+}
+.tram-number {
+  margin-right: .5rem;
+  padding: .5rem;
+  line-height: 1rem;
+  width: 1.25rem;
+}
+
+.stops {
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+}
+
+.icon {
+  margin-right: 2px;
+  height: 1.25rem;
+  background-image: url(../assets/icons/tram.png);
+  background-repeat: no-repeat;
+
+  background-size: contain;
+  background-position: center center;
+  align-self: center;
+  width: 1.25rem;
+}
+</style>
