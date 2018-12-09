70fb8107af9474922e89356b9861b43ae0e9f2e6
Aksels Ledins <aksels.ledins@gmail.com>
2018-12-09 21:19:06 +0100

    (p) +PrivacyPolicy at least kind of

---
+++ src/pages/PrivacyPolicy.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="main-container">
+    <div class="search-form">
+      <h1>
+        nantes
+        <small>.cool</small>
+      </h1>
+    </div>
+    <div>
+      <h2>HyperMoon Privacy Policy</h2>This Privacy Policy describes how your personal information is collected, used, and shared when you visit nantes.cool (the “Site”).
+      <h3>PERSONAL INFORMATION WE COLLECT</h3>
+      <p>
+        When you visit the Site, we automatically collect certain information about your device, including information about your web browser, IP address, time zone, and some of the cookies that are installed on your device. Additionally, as you browse the Site, we collect information about the individual web pages or products that you view, what websites or search terms referred you to the Site, and information about how you interact with the Site. We refer to this automatically-collected information as “Device Information.”
+        We collect Device Information using the following technologies:
+        <ul>
+        <li>“Cookies” are data files that are placed on your device or computer and often include an anonymous unique identifier. For more information about cookies, and how to disable cookies, visit http://www.allaboutcookies.org.</li>
+        <li>“Log files” track actions occurring on the Site, and collect data including your IP address, browser type, Internet service provider, referring/exit pages, and date/time stamps.</li>
+        <li>“Web beacons,” “tags,” and “pixels” are electronic files used to record information about how you browse the Site.</li>
+        <li>Google Analytics</li>
+        </ul>
+        When we talk about “Personal Information” in this Privacy Policy, we are talking about Device Information.
+      </p>
+      <h3>HOW DO WE USE YOUR PERSONAL INFORMATION?</h3>We use the Order Information that we collect generally to fulfill any orders placed through the Site (including processing your payment information, arranging for shipping, and providing you with invoices and/or order confirmations). Additionally, we use this Order Information to:
+      Communicate with you;
+      Screen our orders for potential risk or fraud; and
+      When in line with the preferences you have shared with us, provide you with information or advertising relating to our products or services.
+      We use the Device Information that we collect to help us screen for potential risk and fraud (in particular, your IP address), and more generally to improve and optimize our Site (for example, by generating analytics about how our customers browse and interact with the Site, and to assess the success of our marketing and advertising campaigns).
+      <h3>YOUR RIGHTS</h3>If you are a European resident, you have the right to access personal information we hold about you and to ask that your personal information be corrected, updated, or deleted. If you would like to exercise this right, please contact us through the contact information below.
+      Additionally, if you are a European resident we note that we are processing your information in order to fulfill contracts we might have with you (for example if you make an order through the Site), or otherwise to pursue our legitimate business interests listed above. Additionally, please note that your information will be transferred outside of Europe, including to Canada and the United States.
+      <h3>MINORS</h3>The Site is not intended for individuals under the age of 18.
+      <h3>CHANGES</h3>We may update this privacy policy from time to time in order to reflect, for example, changes to our practices or for other operational, legal or regulatory reasons.
+      <h3>CONTACT US</h3>For more information about our privacy practices, if you have questions, or if you would like to make a complaint, please contact us by e-mail at aksels.ledins@gmail.com.
+    </div>
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+
 import Search from '@/pages/Search';
+import PrivacyPolicy from '@/pages/PrivacyPolicy';
 
 Vue.use(Router);
 
@@ -10,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Search',
       component: Search,
+    },
+    {
+      path: '/privacy-policy',
+      name: 'PrivacyPolicy',
+      component: PrivacyPolicy,
     },
   ],
 });
